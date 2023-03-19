42336fb475c6e3fb332e034867789662af20e48c
HristiyanXO <h.y.aleksandrov@gmail.com>
2023-03-19 11:17:15 +0100

    init

---
+++ Assets/Scripts/Rotation.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+    // public GameObject rightArrow = GameObject.FindWithTag ("rightArrow");
+
+
+    void OnClick(){
+        Debug.Log("Logging Message to Console");
+        Debug.Log("A");
+
+        // rightArrow.SpecialFunction()
+    }
+
+}
